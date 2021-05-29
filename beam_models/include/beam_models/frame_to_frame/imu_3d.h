9717d56ded5796957101e0890a70aee1fda6d026
adthoms <adthoms@uwaterloo.ca>
2021-05-28 20:28:56 -0400

    initial refactoring of Imu3D sensor model

--- beam_models/include/beam_models/frame_to_frame/imu_3d.h
+++ beam_models/include/beam_models/frame_to_frame/imu_3d.h
@@ -11,26 +11,51 @@ class Imu3D
     : public FrameToFrameSensorModelBase<
           sensor_msgs::Imu, beam_parameters::models::Imu3DParams,
           beam_constraints::frame_to_frame::ImuState3DStampedTransaction> {
-public:
+ public:
   SMART_PTR_DEFINITIONS(Imu3D);
 
+  /**
+   * @brief Default constructor
+   */
   Imu3D();
 
+  /**
+   * @brief Destructor
+   */
   ~Imu3D() override = default;
 
+  /**
+   * @brief Callback for transaction generation
+   * @param msg - The IMU message to process
+   */
   beam_constraints::frame_to_frame::ImuState3DStampedTransaction
   GenerateTransaction(const sensor_msgs::Imu::ConstPtr& msg);
 
-protected:
+ protected:
+  /**
+   * @brief Perform required initialization for the sensor model
+   */
   void onInit() override;
 
+  /**
+   * @brief Subscribe to the input topic to start sending transactions to the
+   * optimizer
+   */
   void onStart() override;
 
+  /**
+   * @brief Unsubscribe from the input topic to stop sending transactions to the
+   * optimizer
+   */
   void onStop() override;
 
-  std::unique_ptr<ImuPreintegration> imu_preintegration_;
+  double elapsed_window_time_{0};
+  bool set_start_{true};
+  ros::Time t_prev_;
 
+  std::unique_ptr<ImuPreintegration> imu_preintegration_;
   beam_parameters::models::Imu3DParams params_;
 };
 
-}}  // namespace beam_models::frame_to_frame 
+}  // namespace frame_to_frame
+}  // namespace beam_models
--- beam_models/src/frame_to_frame/imu_3d.cpp
+++ beam_models/src/frame_to_frame/imu_3d.cpp
@@ -7,7 +7,8 @@
 PLUGINLIB_EXPORT_CLASS(beam_models::frame_to_frame::Imu3D,
                        fuse_core::SensorModel)
 
-namespace beam_models { namespace frame_to_frame {
+namespace beam_models {
+namespace frame_to_frame {
 
 Imu3D::Imu3D() : FrameToFrameSensorModelBase() {}
 
@@ -17,34 +18,53 @@ void Imu3D::onInit() {
 
   // init imu preintegration
   ImuPreintegration::Params imu_preintegration_params{
-      .gravitational_acceleration = params_.gravitational_acceleration};
+      .gravitational_acceleration = params_.gravitational_acceleration,
+      .prior_noise = params_.prior_noise,
+      .cov_gyro_noise = params_.cov_gyro_noise,
+      .cov_accel_noise = params_.cov_accel_noise,
+      .cov_gyro_bias = params_.cov_gyro_bias,
+      .cov_accel_bias = params_.cov_accel_bias,
+      .source = name()};
   imu_preintegration_ =
       std::make_unique<ImuPreintegration>(imu_preintegration_params);
 }
 
 void Imu3D::onStart() {
-  imu_preintegration_->ClearBuffer();
   subscriber_ = node_handle_.subscribe(
       base_params_->subscriber_topic, params_.queue_size,
       &ThrottledCallback::callback, &throttled_callback_);
 };
 
-void Imu3D::onStop() {
-  imu_preintegration_->ClearBuffer();
-  subscriber_.shutdown();
-}
+void Imu3D::onStop() { subscriber_.shutdown(); }
 
 beam_constraints::frame_to_frame::ImuState3DStampedTransaction
 Imu3D::GenerateTransaction(const sensor_msgs::Imu::ConstPtr& msg) {
   ROS_DEBUG("Received incoming imu message");
+  ros::Time t_now = msg->header.stamp;
 
-  // need to refactor using Jake's interface
+  // set start IMU state to Orientation = I, Position = 0, Velocity = 0
+  if (set_start_) {
+    imu_preintegration_->SetStart(t_now);
+    set_start_ = false;
+    t_prev_ = t_now;
+  }
 
-  // imu_preintegration_->PopulateBuffer(msg);
-  // if (imu_preintegration_->GetBufferTime() >= params_.max_buffer_time) {
-  //   imu_preintegration_->RegisterNewImuPreintegrationFactor();
-  //   imu_preintegration_->ClearBuffer();
-  // }
+  // increment elapsed window time 
+  elapsed_window_time_ += t_now.toSec() - t_prev_.toSec();
+  t_prev_ = t_now;
+
+  // populate imu buffer
+  imu_preintegration_->PopulateBuffer(msg);
+
+  // generate transaction
+  if (elapsed_window_time_ >= params_.window_time) {
+    elapsed_window_time_ = 0;
+    return imu_preintegration_->RegisterNewImuPreintegratedFactor(t_now);
+  } else {
+    return beam_constraints::frame_to_frame::ImuState3DStampedTransaction(
+        t_now);
+  }
 }
 
-}}  // namespace frame_to_frame
+}  // namespace frame_to_frame
+}  // namespace beam_models
