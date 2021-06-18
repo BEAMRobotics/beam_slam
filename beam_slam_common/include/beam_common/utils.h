875f86408b48c76339b412d1916a85dad6d86690
adthoms <adthoms@uwaterloo.ca>
2021-06-18 19:43:46 -0400

    fix common utils

--- beam_slam_common/include/beam_common/utils.h
+++ beam_slam_common/include/beam_common/utils.h
@@ -3,6 +3,8 @@
 #include <fuse_core/eigen.h>
 #include <fuse_variables/orientation_3d_stamped.h>
 #include <fuse_variables/position_3d_stamped.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <tf/transform_datatypes.h>
 
 namespace beam_common {
 
@@ -23,12 +25,60 @@ inline void EigenTransformToFusePose(const Eigen::Matrix4d& T_WORLD_SENSOR,
   o.w() = q.w();
 }
 
-inline void FusePoseToEigenTransform(const fuse_variables::Position3DStamped& p,
-                                     const fuse_variables::Orientation3DStamped& o,
-                                     Eigen::Matrix4d& T_WORLD_SENSOR) {
+inline void FusePoseToEigenTransform(
+    const fuse_variables::Position3DStamped& p,
+    const fuse_variables::Orientation3DStamped& o,
+    Eigen::Matrix4d& T_WORLD_SENSOR) {
   Eigen::Quaterniond q(o.w(), o.x(), o.y(), o.z());
   T_WORLD_SENSOR.block(0, 3, 3, 1) = Eigen::Vector3d{p.x(), p.y(), p.z()};
   T_WORLD_SENSOR.block(0, 0, 3, 3) = q.toRotationMatrix();
+}
+
+inline Eigen::Matrix4d FusePoseToEigenTransform(
+    const fuse_variables::Position3DStamped& p,
+    const fuse_variables::Orientation3DStamped& o) {
+  Eigen::Matrix4d T = Eigen::Matrix4d::Identity();
+  
+  // add position
+  T(0,3) = p.x();
+  T(1,3) = p.y();
+  T(2,3) = p.z();
+
+  // add rotation
+  Eigen::Quaterniond q(o.w(), o.x(), o.y(), o.z());
+  Eigen::Matrix3d R = q.toRotationMatrix();
+  T.block(0,0,3,3) = R;
+  return T;
+}
+
+inline void ROSStampedTransformToEigenTransform(
+    const tf::StampedTransform& TROS, Eigen::Matrix4d& T) {
+  Eigen::Matrix4f T_float{Eigen::Matrix4f::Identity()};
+  T_float(0, 3) = TROS.getOrigin().getX();
+  T_float(1, 3) = TROS.getOrigin().getY();
+  T_float(2, 3) = TROS.getOrigin().getZ();
+  Eigen::Quaternionf q;
+  q.x() = TROS.getRotation().getX();
+  q.y() = TROS.getRotation().getY();
+  q.z() = TROS.getRotation().getZ();
+  q.w() = TROS.getRotation().getW();
+  T_float.block(0, 0, 3, 3) = q.toRotationMatrix();
+  T = T_float.cast<double>();
+}
+
+inline void GeometryTransformStampedToEigenTransform(
+    const geometry_msgs::TransformStamped& TROS, Eigen::Matrix4d& T) {
+  Eigen::Matrix4f T_float{Eigen::Matrix4f::Identity()};
+  T_float(0, 3) = TROS.transform.translation.x;
+  T_float(1, 3) = TROS.transform.translation.y;
+  T_float(2, 3) = TROS.transform.translation.z;
+  Eigen::Quaternionf q;
+  q.x() = TROS.transform.rotation.x;
+  q.y() = TROS.transform.rotation.y;
+  q.z() = TROS.transform.rotation.z;
+  q.w() = TROS.transform.rotation.w;
+  T_float.block(0, 0, 3, 3) = q.toRotationMatrix();
+  T = T_float.cast<double>();
 }
 
 }  // namespace beam_common
