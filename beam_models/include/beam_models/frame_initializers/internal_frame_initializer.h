c47d9682def6f367243d03a16d9724d15d950259
adthoms <adthoms@uwaterloo.ca>
2021-07-12 13:14:02 -0400

    refactor InternalFrameInitializer into non-singleton and fix build

--- beam_models/include/beam_models/frame_initializers/internal_frame_initializer.h
+++ beam_models/include/beam_models/frame_initializers/internal_frame_initializer.h
@@ -4,59 +4,34 @@
 #include <beam_models/frame_to_frame/imu_preintegration.h>
 #include <beam_utils/utils.h>
 
-namespace beam_models { namespace frame_initializers {
+namespace beam_models {
+namespace frame_initializers {
 
 /**
  * @brief This class can be used to estimate a pose of a frame given its
- * timestamp. This is done by building searching through the current fuse graph,
+ * timestamp. This is done by searching through the current fuse graph,
  * or preintegrating to the time point
  */
 class InternalFrameInitializer : public FrameInitializerBase {
-public:
+ public:
   /**
-   * @brief Static Instance getter (singleton)
-   * @return reference to the singleton
+   * @brief Constructor
+   * @param poses_buffer_time length of time (in seconds) to store poses for
+   * interpolation
+   * @param sensor_frame_id frame ID attached to the sensor. See
+   * FrameInitializerBase for description
    */
-  static InternalFrameInitializer& GetInstance();
-
-  /**
-   * @brief Gets estimate frame pose (in world frame in imu coords)
-   * @param time stamp of the frame being initialized
-   * @param T_WORLD_SENSOR reference to result
-   * @param frame_id id of the desired output frame
-   * @return true if pose lookup was successful
-   */
-  bool GetEstimatedPose(const ros::Time& time, Eigen::Matrix4d& T_WORLD_SENSOR,
-                        std::string frame_id = "") override;
+  InternalFrameInitializer(int64_t poses_buffer_time,
+                           const std::string& sensor_frame_id = "");
 
   /**
    * @brief Helper function to add an orientation variable
-   * @param stamp timestamp of orientation to add
    * @param R_WORLD_SENSOR orientation variable to add
-   * @param frame_id id of the desired input frame
+   * @param stamp timestamp of orientation to add
+   * @return true if pose was added successfully
    */
-  void AddPose(const ros::Time& stamp, const Eigen::Matrix4d& T_WORLD_SENSOR,
-               std::string frame_id);
-
-  /**
-   * @brief Delete copy constructor
-   */
-  InternalFrameInitializer(InternalFrameInitializer const&) = delete;
-
-  /**
-   * @brief Delete assignment constructor
-   */
-  void operator=(InternalFrameInitializer const&) = delete;
-
-private:
-  /**
-   * @brief Private Default Constructor
-   */
-  InternalFrameInitializer() {}
-
-  beam_common::PoseLookup::Params pose_lookup_params_;
-  std::unique_ptr<beam_common::PoseLookup> pose_lookup_;
-  std::shared_ptr<tf2::BufferCore> poses_;
+  void AddPose(const Eigen::Matrix4d& T_WORLD_SENSOR, const ros::Time& stamp);
 };
 
-}} // namespace beam_models::frame_initializers
+}  // namespace frame_initializers
+}  // namespace beam_models
--- beam_models/src/frame_initializers/internal_frame_initializer.cpp
+++ beam_models/src/frame_initializers/internal_frame_initializer.cpp
@@ -3,61 +3,36 @@
 namespace beam_models {
 namespace frame_initializers {
 
-InternalFrameInitializer& InternalFrameInitializer::GetInstance() {
-  static InternalFrameInitializer instance;
-  return instance;
+InternalFrameInitializer::InternalFrameInitializer(
+    int64_t poses_buffer_time, const std::string& sensor_frame_id)
+    : FrameInitializerBase(sensor_frame_id) {
+  poses_ = std::make_shared<tf2::BufferCore>(ros::Duration(poses_buffer_time));
+  pose_lookup_.SetPoses(poses_);
 }
 
-bool InternalFrameInitializer::GetEstimatedPose(const ros::Time& time,
-                                                Eigen::Matrix4d& T_WORLD_SENSOR,
-                                                std::string frame_id) {
-  if (pose_lookup_ == nullptr) {
-    return false;
-  }
-  // set to desired frame id
-  pose_lookup_params_.sensor_frame = frame_id;
-  bool result = pose_lookup_->GetT_WORLD_SENSOR(T_WORLD_SENSOR, time);
-  return result;
-}
-
-InternalFrameInitializer::AddPose(const ros::Time& stamp,
-                                  const Eigen::Matrix4d& T_WORLD_SENSOR,
-                                  std::string frame_id) {
-  if (poses_ == nullptr) {
-    poses_ =
-        std::make_shared<tf2::BufferCore>(ros::Duration(poses_buffer_time));
-    pose_lookup_params_.poses = poses_;
-  }
-  if (pose_lookup_params_.baselink_frame.empty()) {
-    pose_lookup_params_.baselink_frame = extrinsics->baselink();
-  }
-  if (pose_lookup_params_.world_frame.empty()) {
-    pose_lookup_params_.world_frame = extrinsics->world_frame();
-  }
-  if (pose_lookup_ == nullptr) {
-    pose_lookup_ =
-        std::make_unique<beam_common::PoseLookup>(pose_lookup_params_);
-  }
-
+void InternalFrameInitializer::AddPose(const Eigen::Matrix4d& T_WORLD_SENSOR,
+                                       const ros::Time& stamp) {
   // convert matrix to position and quaternion
   Eigen::Quaterniond orientation;
   Eigen::Vector3d position;
   beam::TransformMatrixToQuaternionAndTranslation(T_WORLD_SENSOR, orientation,
                                                   position);
+
   // build tf message and populate in buffer core
   geometry_msgs::TransformStamped tf_stamped;
   tf_stamped.header.stamp = stamp;
-  tf_stamped.header.frame_id = "/" + extrinsics->world_frame();
-  tf_stamped.child_frame_id = frame_id;
-  tf_stamped.transform.translation.x = position.x;
-  tf_stamped.transform.translation.y = position.y;
-  tf_stamped.transform.translation.z = position.z;
-  tf_stamped.transform.rotation.x = orientation.x;
-  tf_stamped.transform.rotation.y = orientation.y;
-  tf_stamped.transform.rotation.z = orientation.z;
-  tf_stamped.transform.rotation.w = orientation.w;
+  tf_stamped.header.frame_id = pose_lookup_.GetWorldFrameID();
+  tf_stamped.child_frame_id = pose_lookup_.GetBaselinkFrameID();
+  tf_stamped.transform.translation.x = position[0];
+  tf_stamped.transform.translation.y = position[1];
+  tf_stamped.transform.translation.z = position[2];
+  tf_stamped.transform.rotation.w = orientation.w();
+  tf_stamped.transform.rotation.x = orientation.x();
+  tf_stamped.transform.rotation.y = orientation.y();
+  tf_stamped.transform.rotation.z = orientation.z();
   std::string authority{"odometry"};
   poses_->setTransform(tf_stamped, authority, false);
 }
+
 }  // namespace frame_initializers
 }  // namespace beam_models
