aec7afa81f2188d5d1d6f20b6dc6b64e7728abde
Jake McLaughlin <jake.mclaughlin98@gmail.com>
2021-07-20 14:06:50 -0400

    fix utils

--- beam_slam_common/include/beam_common/utils.h
+++ beam_slam_common/include/beam_common/utils.h
@@ -6,8 +6,8 @@
 #include <fuse_variables/position_3d_stamped.h>
 #include <geometry_msgs/TransformStamped.h>
 #include <nav_msgs/Odometry.h>
-#include <tf/transform_datatypes.h>
 #include <nav_msgs/Path.h>
+#include <tf/transform_datatypes.h>
 
 #include <beam_common/scan_pose.h>
 #include <beam_matching/loam/LoamPointCloud.h>
@@ -24,75 +24,19 @@ void FusePoseToEigenTransform(const fuse_variables::Position3DStamped& p,
                               const fuse_variables::Orientation3DStamped& o,
                               Eigen::Matrix4d& T_WORLD_SENSOR);
 
-Eigen::Matrix4d
-    FusePoseToEigenTransform(const fuse_variables::Position3DStamped& p,
-                             const fuse_variables::Orientation3DStamped& o);
+Eigen::Matrix4d FusePoseToEigenTransform(
+    const fuse_variables::Position3DStamped& p,
+    const fuse_variables::Orientation3DStamped& o);
 
-/**
- * @brief Turns a pose message into an Eigen 4x4 matrix
- * @param pose pose message to turn into eigen matrix
- * @param T_WORLD_SENSOR[out] Transform to return
- */
-inline void
-    PoseMsgToTransformationMatrix(const geometry_msgs::PoseStamped& pose,
-                                  Eigen::Matrix4d& T_WORLD_SENSOR) {
-  Eigen::Vector3d position;
-  position[0] = pose.pose.position.x;
-  position[1] = pose.pose.position.y;
-  position[2] = pose.pose.position.z;
-  Eigen::Quaterniond orientation;
-  orientation.w() = pose.pose.orientation.w;
-  orientation.x() = pose.pose.orientation.x;
-  orientation.y() = pose.pose.orientation.y;
-  orientation.z() = pose.pose.orientation.z;
-  beam::QuaternionAndTranslationToTransformMatrix(orientation, position,
-                                                  T_WORLD_SENSOR);
-}
+void PoseMsgToTransformationMatrix(const geometry_msgs::PoseStamped& pose,
+                                   Eigen::Matrix4d& T_WORLD_SENSOR);
 
+void OdometryMsgToTransformationMatrix(const nav_msgs::Odometry& odom,
+                                       Eigen::Matrix4d& T_WORLD_SENSOR);
 
-/**
- * @brief Turns a pose message into an Eigen 4x4 matrix
- * @param pose pose message to turn into eigen matrix
- * @param T_WORLD_SENSOR[out] Transform to return
- */
-inline void TransformationMatrixToPoseMsg(const Eigen::Matrix4d& T_WORLD_SENSOR,
-                                          const ros::Time& stamp,
-                                          geometry_msgs::PoseStamped& pose) {
-  Eigen::Vector3d position;
-  Eigen::Quaterniond orientation;
-  beam::TransformMatrixToQuaternionAndTranslation(T_WORLD_SENSOR, orientation,
-                                                  position);
-  pose.header.stamp = stamp;
-  pose.pose.position.x = position[0];
-  pose.pose.position.y = position[1];
-  pose.pose.position.z = position[2];
-  pose.pose.orientation.w = orientation.w();
-  pose.pose.orientation.x = orientation.x();
-  pose.pose.orientation.y = orientation.y();
-  pose.pose.orientation.z = orientation.z();
-}
-
-/**
- * @brief Interpolates a pose given a list of poses and a time
- * @param poses list of poses
- * @param time time to interpolate pose for
- * @param T_WORLD_SENSOR[out] pose to return
- */
-inline void InterpolateTransformFromPath(
-    const std::vector<geometry_msgs::PoseStamped>& poses, const ros::Time& time,
-    Eigen::Matrix4d& T_WORLD_SENSOR) {
-  for (int i = 0; i < poses.size(); i++) {
-    if (time < poses[i + 1].header.stamp && time >= poses[i].header.stamp) {
-      Eigen::Matrix4d pose1, pose2;
-      PoseMsgToTransformationMatrix(poses[i], pose1);
-      PoseMsgToTransformationMatrix(poses[i + 1], pose2);
-      T_WORLD_SENSOR = beam::InterpolateTransform(
-          pose1, beam::RosTimeToChrono(poses[i].header.stamp), pose2,
-          beam::RosTimeToChrono(poses[i + 1].header.stamp),
-          beam::RosTimeToChrono(time));
-    }
-  }
-}
+void InterpolateTransformFromPath(const nav_msgs::Path& path,
+                                  const ros::Time& time,
+                                  Eigen::Matrix4d& T_WORLD_SENSOR);
 
 /**
  * @brief iterates through all keypoints in the list and add up the change in
@@ -103,4 +47,39 @@ inline void InterpolateTransformFromPath(
 double CalculateTrajectoryLength(
     const std::list<beam_common::ScanPose>& keyframes);
 
+void ROSStampedTransformToEigenTransform(const tf::StampedTransform& TROS,
+                                         Eigen::Matrix4d& T);
+
+void TransformStampedMsgToEigenTransform(
+    const geometry_msgs::TransformStamped& TROS, Eigen::Matrix4d& T);
+
+void EigenTransformToTransformStampedMsg(
+    const Eigen::Matrix4d& T, const ros::Time& stamp, int seq,
+    const std::string& parent_frame_id, const std::string& child_frame_id,
+    geometry_msgs::TransformStamped& tf_stamped);
+
+void OdometryMsgToTransformedStamped(
+    const nav_msgs::Odometry& message, const ros::Time& stamp, int seq,
+    const std::string& parent_frame_id, const std::string& child_frame_id,
+    geometry_msgs::TransformStamped& tf_stamped);
+
+/**
+ * @brief Turns a pose message into an Eigen 4x4 matrix
+ * @param pose pose message to turn into eigen matrix
+ * @param T_WORLD_SENSOR[out] Transform to return
+ */
+void TransformationMatrixToPoseMsg(const Eigen::Matrix4d& T_WORLD_SENSOR,
+                                   const ros::Time& stamp,
+                                   geometry_msgs::PoseStamped& pose);
+
+/**
+ * @brief Interpolates a pose given a list of poses and a time
+ * @param poses list of poses
+ * @param time time to interpolate pose for
+ * @param T_WORLD_SENSOR[out] pose to return
+ */
+void InterpolateTransformFromPath(
+    const std::vector<geometry_msgs::PoseStamped>& poses, const ros::Time& time,
+    Eigen::Matrix4d& T_WORLD_SENSOR);
+    
 } // namespace beam_common
