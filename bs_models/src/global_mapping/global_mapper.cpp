9850d12c452d250dacbcb622fe6f5e7816dba74e
nickcharron <nicholas.c.charron@gmail.com>
2021-08-04 00:07:41 -0400

    fix error in transform reading

--- bs_models/src/global_mapping/global_mapper.cpp
+++ bs_models/src/global_mapping/global_mapper.cpp
@@ -22,7 +22,7 @@ GlobalMapper::GlobalMapper()
 void GlobalMapper::process(const SlamChunkMsg::ConstPtr& msg) {
   ros::Time stamp = msg->stamp;
   std::vector<float> T = msg->T_WORLD_BASELINK;
-  Eigen::Matrix4d T_WORLD_BASELINK = beam::VectorToEigenTransform(T);
+  Eigen::Matrix4d T_WORLD_BASELINK = VectorToTransform(T);
 
   fuse_core::Transaction::SharedPtr new_transaction =
       global_map_->AddMeasurement(
