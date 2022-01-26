4fa7e5dbfc2426f57dae69128dd94f33daf5989e
Jake McLaughlin <jake.mclaughlin98@gmail.com>
2022-01-26 17:17:11 -0600

    Fix default for pose prior covariance

--- bs_common/include/bs_parameters/models/vio_params.h
+++ bs_common/include/bs_parameters/models/vio_params.h
@@ -84,9 +84,11 @@ public:
              prior_diagonal);
     if (prior_diagonal.size() != 6) {
       ROS_ERROR("Invalid gm_noise_diagonal params, required 6 params, "
-                "given: %d. Using default (0.1 for all)",
+                "given: %d. Using default (0.00000000001 for all)",
                 prior_diagonal.size());
-      prior_diagonal = std::vector<double>{0.1, 0.1, 0.1, 0.1, 0.1, 0.1};
+      prior_diagonal =
+          std::vector<double>{0.00000000001, 0.00000000001, 0.00000000001,
+                              0.00000000001, 0.00000000001, 0.00000000001};
     }
     if (std::accumulate(prior_diagonal.begin(), prior_diagonal.end(), 0.0) ==
         0.0) {
