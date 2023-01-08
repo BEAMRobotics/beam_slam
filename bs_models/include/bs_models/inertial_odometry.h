df96b6cbf7d73fa8b92686a91a1740a7de0a5ba8
Jake McLaughlin <jake.mclaughlin98@gmail.com>
2023-01-08 16:46:27 -0500

    Add skeleton code for inertial odometry

--- bs_models/include/bs_models/inertial_odometry.h
+++ bs_models/include/bs_models/inertial_odometry.h
@@ -2,6 +2,7 @@
 
 #include <queue>
 
+<<<<<<< HEAD
 #include <bs_common/bs_msgs.h>
 
 #include <fuse_core/async_sensor_model.h>
@@ -10,6 +11,18 @@
 #include <sensor_msgs/Imu.h>
 
 #include <bs_common/extrinsics_lookup_online.h>
+=======
+#include <fuse_core/async_sensor_model.h>
+#include <fuse_core/macros.h>
+#include <fuse_core/throttled_callback.h>
+#include <fuse_graphs/hash_graph.h>
+#include <nav_msgs/Odometry.h>
+#include <sensor_msgs/Imu.h>
+
+#include <bs_common/bs_msgs.h>
+#include <bs_common/extrinsics_lookup_online.h>
+#include <bs_models/frame_initializers/frame_initializers.h>
+>>>>>>> Add skeleton code for inertial odometry
 #include <bs_models/imu/imu_preintegration.h>
 #include <bs_parameters/models/calibration_params.h>
 #include <bs_parameters/models/inertial_odometry_params.h>
@@ -20,7 +33,11 @@ using namespace bs_common;
 
 class InertialOdometry : public fuse_core::AsyncSensorModel {
 public:
+<<<<<<< HEAD
   FUSE_SMART_PTR_DEFINITIONS(InertialOdometry);
+=======
+  SMART_PTR_DEFINITIONS(InertialOdometry);
+>>>>>>> Add skeleton code for inertial odometry
 
   /**
    * @brief Default Constructor
@@ -41,6 +58,7 @@ private:
   void processIMU(const sensor_msgs::Imu::ConstPtr& msg);
 
   /**
+<<<<<<< HEAD
    * @brief Callback for path processing, this path is provided by LIO for
    * initialization
    * @param[in] msg - The path to process
@@ -50,6 +68,22 @@ private:
   /**
    * @brief Perform any required initialization for the sensor model
    * (Load parameters from yaml files and read in imu intrinsics)
+=======
+   * @brief Callback for processing odometry messages, these messages are meant to be poses in which
+   * we add constraints to
+   * @param[in] msg - The odom msg to process
+   */
+  void processOdometry(const nav_msgs::Odometry::ConstPtr& msg);
+
+  /**
+   * @brief Perform any required initialization for the sensor model
+   *
+   * This could include things like reading from the parameter server or
+   * subscribing to topics. The class's node handles will be properly
+   * initialized before SensorModel::onInit() is called. Spinning of the
+   * callback queue will not begin until after the call to SensorModel::onInit()
+   * completes.
+>>>>>>> Add skeleton code for inertial odometry
    */
   void onInit() override;
 
@@ -60,6 +94,7 @@ private:
   void onStart() override;
 
   /**
+<<<<<<< HEAD
    * @brief Unsubscribe to the input topic
    */
   void onStop() override;
@@ -112,6 +147,52 @@ private:
   // robot extrinsics
   bs_common::ExtrinsicsLookupOnline& extrinsics_ =
       bs_common::ExtrinsicsLookupOnline::GetInstance();
+=======
+   * @brief Unsubscribe to the input topics
+   */
+  void onStop() override {}
+
+  /**
+   * @brief Read in graph updates
+   */
+  void onGraphUpdate(fuse_core::Graph::ConstSharedPtr graph_msg) override;
+
+  fuse_core::UUID device_id_; //!< The UUID of this device
+
+  bool initialized{false};
+  // calibration parameters
+  bs_parameters::models::CalibrationParams calibration_params_;
+
+  // loadable parameters
+  bs_parameters::models : InertialOdometryParams params_;
+
+  // subscribers
+  ros::Subscriber imu_subscriber_;
+  ros::Subscriber odom_subscriber_;
+
+  // publishers
+  ros::Publisher odom_publisher_;
+
+  // data storage
+  std::queue<sensor_msgs::Imu> imu_buffer_;
+
+  // primary odom objects
+  std::shared_ptr<ImuPreintegration> imu_preint_;
+  bs_models::ImuPreintegration::Params imu_params_;
+  std::unique_ptr<frame_initializers::FrameInitializerBase> frame_initializer_;
+
+  // extrinsics
+  Eigen::Matrix4d T_imu_baselink_;
+  bs_common::ExtrinsicsLookupOnline& extrinsics_ = bs_common::ExtrinsicsLookupOnline::GetInstance();
+
+  // throttled callbacks for imu
+  using ThrottledIMUCallback = fuse_core::ThrottledMessageCallback<sensor_msgs::Imu>;
+  ThrottledIMUCallback throttled_imu_callback_;
+
+  // throttle callback for odometry
+  using ThrottledOdomCallback = fuse_core::ThrottledMessageCallback<nav_msgs::Odometry>;
+  ThrottledOdomCallback throttled_odom_callback_;
+>>>>>>> Add skeleton code for inertial odometry
 };
 
 } // namespace bs_models
